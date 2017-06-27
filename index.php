2ca90322734fb84c5251820ba0596b690992166f
sbociot <stevebocicot@icloud.com>
2017-06-27 14:27:23 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php require "views/header.php";?>
+<nav class="header_main_menu">
+  <div class="logo_container">
+    <img src="images/logo.png" alt="SMS Concours">
+  </div>
+  <h1>1000 Étudiant inscrits</h1>
+  <ul class="menu">
+    <li>
+      <a href="#">Services</a>
+      <div class="dropdown_service">
+        <ul class="service">
+          <li><a href="#">SMS Concours</a></li>
+          <li><a href="#">Biletain en ligne</a></li>
+        </ul>
+      </div>
+    </li>
+    <li><a href="#">Inscription</a></li>
+    <li><a href="#">Connexion</a></li>
+    <li>
+      <a href="#">Pays</a>
+      <div class="dropdown_pays">
+        <ul class="service">
+          <li><a href="#">Senegal</a></li>
+          <li><a href="#">Mali</a></li>
+          <li><a href="#">Burkani</a></li>
+          <li><a href="#">Guinée</a></li>
+          <li><a href="#">Côte d'Ivoire</a></li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</nav>
+
+
+<?php require "views/footer.php"; ?>
---
+++ views/header.php
@@ -0,0 +1,19 @@
+<?php define('IMG', 'images/'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="style/main.css">
+<!--[if IE 8]>
+  <script type="text/javascript">
+    // this is only executed for IE 8
+  </script>
+<![endif]-->
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>
