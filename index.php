874142dfea77bab73f5b12da6f33287db7a29a38
Kevin Duvignau <kevin.duvignau40@gmail.com>
2017-06-28 10:33:51 -0400

    header structure

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
 <?php require "views/header.php";?>
-<nav class="header_main_menu">
-  <div class="logo_container">
-    <img src="images/logo.png" alt="SMS Concours">
+<header>
+  <div class="main_header_content">
+    <nav class="header_main_menu">
+      <ul class="main_logo">
+        <li><a href="#"><img src="images/logo.png" alt="SMS Concours"></a></li>
+        <li><a href="#">1000 Étudiants Inscrits</a></li>
+      </ul>
+      <ul class="main_menu">
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Inscription</a></li>
+        <li><a href="#">Connexion</a></li>
+        <li><a href="#">Pays</a></li>
+      </ul>
+    </nav>
   </div>
-  <h1>1000 Étudiant inscrits</h1>
-  <ul class="menu">
-    <li>
-      <a href="#">Services</a>
-      <div class="dropdown_service">
-        <ul class="service">
-          <li><a href="#">SMS Concours</a></li>
-          <li><a href="#">Biletain en ligne</a></li>
-        </ul>
-      </div>
-    </li>
-    <li><a href="#">Inscription</a></li>
-    <li><a href="#">Connexion</a></li>
-    <li>
-      <a href="#">Pays</a>
-      <div class="dropdown_pays">
-        <ul class="service">
-          <li><a href="#">Senegal</a></li>
-          <li><a href="#">Mali</a></li>
-          <li><a href="#">Burkani</a></li>
-          <li><a href="#">Guinée</a></li>
-          <li><a href="#">Côte d'Ivoire</a></li>
-        </ul>
-      </div>
-    </li>
-  </ul>
-</nav>
+</header>
+<div class="main_content">
+  
+</div>
 
 
 <?php require "views/footer.php"; ?>
